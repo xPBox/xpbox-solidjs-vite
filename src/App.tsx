b05d02e953e2a1de16324441b2a1388e2e23fa95
xPBox <opensource@xpbox.org>
2024-06-05 23:09:45 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+import Main from './Main'
+
+const App: Component = () => {
+  return (
+    <Main />
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile'
+import solidPlugin from 'vite-plugin-solid';
+// import devtools from 'solid-devtools/vite';
+
+export default defineConfig({
+  plugins: [
+    /*
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
+    // devtools(),
+    solidPlugin(),
+    viteSingleFile(),
+  ],
+  build: {
+    target: 'esnext',
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
+});
